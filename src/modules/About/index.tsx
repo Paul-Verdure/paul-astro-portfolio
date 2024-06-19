2a26972800f0953781527aa599554b8aa0db1050
Paul Verdure <paul.verdure@gmail.com>
2024-06-19 09:52:24 +0200

    feat: change about content

--- src/modules/About/index.tsx
+++ src/modules/About/index.tsx
@@ -7,121 +7,110 @@ import { TextBox } from "@components/textBox";
 import { FadeIn } from "@utils/animations/FadeIn";
 
 type AboutProps = {
-    hideExp?: boolean;
+  hideExp?: boolean;
 };
 
 // Hide exp is for hiding the commercial experience - on the about page cuz there is a separate section for that
 export const About: FC<AboutProps> = ({ hideExp }) => {
-    return (
-        <S.AboutStyled>
-            <Container>
-                <FadeIn>
-                    <S.AboutTitle>About me</S.AboutTitle>
-                </FadeIn>
-                <S.AboutContent>
-                    <S.AboutContentText>
-                        <FadeIn>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Quisquam voluptates,
-                                voluptate, doloremque, quas quia quos quidem
-                                exercitationem nemo tempore voluptatum
-                                doloribus? Quisquam voluptates, voluptate,
-                                doloremque, quas quia quos quidem exercitationem
-                                nemo tempore voluptatum doloribus?
-                            </p>
+  return (
+    <S.AboutStyled>
+      <Container>
+        <FadeIn>
+          <S.AboutTitle>About me</S.AboutTitle>
+        </FadeIn>
+        <S.AboutContent>
+          <S.AboutContentText>
+            <FadeIn>
+              <p>
+                As a Software Engineer, I specialize in crafting intuitive user
+                interfaces that meet user expectations. I thrive on solving
+                complex challenges and excel in technical communication, adeptly
+                conveying application intricacies to diverse stakeholders.
+                <br />
+                My educational background includes a Computer Software
+                Engineering degree from Wild Code School and a Master's in
+                Management from KEDGE BS, providing me with a blend of technical
+                expertise and strategic business insight.
+              </p>
 
-                            <h3>What I can do?</h3>
-                            <p>
-                                Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Quisquam voluptates,
-                                voluptate, doloremque, quas quia quos quidem
-                                exercitationem nemo.
-                            </p>
-                        </FadeIn>
-                        <FadeIn>
-                            <S.AboutContentBoxesWrapper>
-                                <TextBox>
-                                    <h4>Web Design</h4>
-                                    <p>
-                                        Lorem ipsum dolor sit amet consectetur
-                                        adipisicing elit.
-                                    </p>
-                                </TextBox>
-                                <TextBox>
-                                    <h4>Web Development</h4>
-                                    <p>
-                                        Lorem ipsum dolor sit amet consectetur
-                                        adipisicing elit.
-                                    </p>
-                                </TextBox>
+              <h3>What I can do?</h3>
+              <p>
+                I build applications such as data visualization dashboards,
+                e-commerce websites, and landing pages. I am proficient in
+                React, JavaScript, and Next.js, and I have experience with Figma
+                for UI/UX design. I am passionate about creating user-friendly
+                interfaces that are visually appealing and easy to navigate. I
+                am always eager to learn new technologies and expand my skill
+                set.
+              </p>
+            </FadeIn>
+            <FadeIn>
+              <S.AboutContentBoxesWrapper>
+                <TextBox>
+                  <h4>Web Development</h4>
+                  <p>
+                    From scratch to deployment, I can build a web application that meets your needs. I can also maintain and update existing applications.
+                  </p>
+                </TextBox>
+                <TextBox>
+                  <h4>UX/UI</h4>
+                  <p>
+                    I can propose and implement design solutions that are both user-friendly and visually appealing.
+                  </p>
+                </TextBox>
 
-                                <TextBox>
-                                    <h4>UI/UX Design</h4>
-                                    <p>
-                                        Lorem ipsum dolor sit amet consectetur
-                                        adipisicing elit.
-                                    </p>
-                                </TextBox>
+                <TextBox>
+                  <h4>Problem-solving</h4>
+                  <p>
+                    I can analyze complex problems and develop innovative solutions to meet your business needs.
+                  </p>
+                </TextBox>
 
-                                <TextBox>
-                                    <h4>Graphic Design</h4>
-                                    <p>
-                                        Lorem ipsum dolor sit amet consectetur
-                                        adipisicing elit.
-                                    </p>
-                                </TextBox>
-                            </S.AboutContentBoxesWrapper>
-                        </FadeIn>
-                        {!hideExp && (
-                            <FadeIn>
-                                <h3>What is my commercial experience?</h3>
-                                <S.AboutContentBoxesWrapper>
-                                    <TextBox
-                                        variant="background-text"
-                                        bgText="senior"
-                                    >
-                                        <h4>React</h4>
-                                        <p>3,5 years</p>
-                                    </TextBox>
-                                    <TextBox
-                                        variant="background-text"
-                                        bgText="mid"
-                                    >
-                                        <h4>JavaScript</h4>
-                                        <p>2 years</p>
-                                    </TextBox>
-                                    <TextBox
-                                        variant="background-text"
-                                        bgText="junior"
-                                    >
-                                        <h4>Next.js</h4>
-                                        <p>1 year</p>
-                                    </TextBox>
-                                    <TextBox
-                                        variant="background-text"
-                                        bgText="new"
-                                    >
-                                        <h4>Figma</h4>
-                                        <p>few months</p>
-                                    </TextBox>
-                                </S.AboutContentBoxesWrapper>
-                                <Button link="/about">view projects</Button>
-                            </FadeIn>
-                        )}
-                    </S.AboutContentText>
-                    <S.AboutContentImage>
-                        <FadeIn delay={0.3}>
-                            <Image
-                                srcLocal="paulImg"
-                                alt="paul verdure"
-                                width={500}
-                                height={500}
-                            />
-                        </FadeIn>
-                    </S.AboutContentImage>
-                </S.AboutContent>
-            </Container>
-        </S.AboutStyled>
-    );
+                <TextBox>
+                  <h4>Communication</h4>
+                  <p>
+                    I can effectively communicate technical information to non-technical stakeholders.
+                  </p>
+                </TextBox>
+              </S.AboutContentBoxesWrapper>
+            </FadeIn>
+            {!hideExp && (
+              <FadeIn>
+                <h3>What is my commercial experience?</h3>
+                <S.AboutContentBoxesWrapper>
+                  <TextBox variant="background-text" bgText="mid">
+                    <h4>React</h4>
+                    <p>2,5 years</p>
+                  </TextBox>
+                  <TextBox variant="background-text" bgText="mid">
+                    <h4>JavaScript & Typescript</h4>
+                    <p>2,5 years</p>
+                  </TextBox>
+                  <TextBox variant="background-text" bgText="junior">
+                    <h4>Next.js</h4>
+                    <p>1 year</p>
+                  </TextBox>
+                  <TextBox variant="background-text" bgText="new">
+                    <h4>Figma</h4>
+                    <p>few months</p>
+                  </TextBox>
+                </S.AboutContentBoxesWrapper>
+                <Button link="/about">view projects</Button>
+              </FadeIn>
+            )}
+          </S.AboutContentText>
+          <S.AboutContentImage>
+            <FadeIn delay={0.3}>
+              <Image
+                srcLocal="paulImg"
+                alt="paul verdure"
+                width={500}
+                height={500}
+              />
+            </FadeIn>
+          </S.AboutContentImage>
+        </S.AboutContent>
+      </Container>
+    </S.AboutStyled>
+  );
 };
